37ee822f31c3ae8fde042b9382c0cc7dd60b1796
Arthur <arthurfreitasdearaujo@outlook.com>
2025-02-02 14:15:27 -0300

    Python

---
+++ ex041.py
@@ -0,0 +1,13 @@
+idade = int(input('Digite a idade:'))
+if idade <=9:
+    print('Até 9 anos: MIRIM')
+elif idade <= 14 and idade > 9:
+    print('Até 14 anos: INFANTIL')
+elif idade <=19 and idade >14:
+    print('Até 19 anos: JUNIOR')
+elif idade <=20 and idade >19:
+    print('Até 20 anos : SÊNIOR')
+
+
+
+
