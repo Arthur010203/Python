9b5489f1420bf6d9d7323eb24849726a233da337
Arthur <arthurfreitasdearaujo@outlook.com>
2025-04-17 16:40:49 -0300

    Python

--- ex057.py
+++ ex057.py
@@ -1,12 +1,19 @@
-def obter_sexo():
-    opcoes_validas = {'M', 'F', 'B', 'NB'}
-    while True:
+'''def obter_sexo():
+    opcoes_validas = {'M', 'F', 'B', 'NB','@', 'X' }
+    while True:k
         sexo = input("Digite o sexo (M, F, B, NB): ").strip().upper()
         if sexo in opcoes_validas:
             return sexo
         else:
-            print("Entrada inválida! Por favor, digite apenas 'M', 'F', 'B' ou 'NB'.")
+            print("Entrada inválida! Por favor, digite apenas 'M', 'F', 'B', '@', 'X' ou 'NB'.")
 
 # Chamando a função
 genero = obter_sexo()
-print(f"Você selecionou: {genero}")
+print(f"Você selecionou: {genero}")'''
+
+# Versão do professor Guanabara
+
+sexo = str(input('Informe seu sexo: [M/F/@/X/NB/B]')).strip().upper()[0]
+while sexo not in 'MmFf@XxNBnbBb':
+    sexo = str(input('Dadaos inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
