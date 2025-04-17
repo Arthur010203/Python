2734c0ef66b106573f921cc99c22d126c6b0efa9
Arthur <arthurfreitasdearaujo@outlook.com>
2025-04-17 16:56:23 -0300

    Python

--- ex058.py
+++ ex058.py
@@ -1,4 +1,4 @@
-import random
+'''import random
 
 def jogo_adivinhacao():
     numero_secreto = random.randint(0, 10)
@@ -22,3 +22,24 @@ def jogo_adivinhacao():
             print("Por favor, digite um número válido.")
 # Chamado a função
 jogo_adivinhacao()
+'''
+
+# Versão Guanabara
+
+from random import randint
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10')
+print('Será que você consegue advinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite?'))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas '.format(palpites))
