6e30735c6fbfb646d3b4366e07e905cfa0e5eb79
Arthur <arthurfreitasdearaujo@outlook.com>
2025-04-28 19:30:15 -0300

    Python

--- ex065.py
+++ ex065.py
@@ -1,4 +1,4 @@
-# Programa para ler vários números e calcular média, maiore menor valor
+''''# Programa para ler vários números e calcular média, maiore menor valor
 
 soma = 0
 quantidade = 0
@@ -29,4 +29,24 @@ media = soma / quantidade
 
 print(f'\nVocê digitou {quantidade} números.')
 print(f"A média foi {media:.2f}.")
-print(f"O maior valor foi {maior} e o menor foi {menor}.")
+print(f"O maior valor foi {maior} e o menor foi {menor}.")'''
+
+# Versão do Guanabara
+
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while resp in 'Ss':
+    núm = int(input('Digite um número: '))
+    soma += núm
+    quant += 1
+    if quant == 1:
+        maior = menor = núm
+    else:
+        if núm > maior:
+            marior = núm
+        if núm < menor:
+            menor = núm
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+média = soma / quant
+print('Você digitou {} números e a média foi {}'.format(quant, média))
+print('O maior valor foi {} e o menor foi {}'.format(maior , menor))
