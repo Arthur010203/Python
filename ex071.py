a40b8b8fe99bc930cb84a407acf546c375d17e7d
Arthur <arthurfreitasdearaujo@outlook.com>
2025-05-16 14:00:34 -0300

    Python

--- ex071.py
+++ ex071.py
@@ -1,4 +1,4 @@
-print("==== CAIXA ELETRÔNICO ====")
+'''print("==== CAIXA ELETRÔNICO ====")
 
 # Solicita o valor a ser sacado
 valor = int(input("Informe o valor a ser sacado: R$ "))
@@ -13,4 +13,34 @@ for cedula in cedulas:
     if quantidade > 0:
         print(f"{quantidade} cédula(s) de R$ {cedula} ")
 
-print("=== FIM DA OPERAÇÃO ===")
+print("=== FIM DA OPERAÇÃO ===")'''
+
+# Versão do Guanabara
+
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))
+print('=' * 30)
+valor = int(input('Que valoar você quer sacar? R$'))
+total = valor
+céd = 50
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+         print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
+
