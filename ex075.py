657c9d34199f5f80d14216f13074e4788c7a6ce2
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-07 21:42:14 -0300

    Python

--- ex075.py
+++ ex075.py
@@ -1,4 +1,4 @@
-# Lê quatro valores do teclado e armazena na tupla
+''''# Lê quatro valores do teclado e armazena na tupla
 valores = tuple(int(input(f"Digite o {i+1} número:")) for i in range(4))
 
 # Mostra a tupla completa
@@ -17,4 +17,21 @@ else:
 
 # C) Quais foram os números pares
 pares = tuple(valor for valor in valores if valor % 2 == 0)
-print(f"C) Os números pares digitados foram: {pares if pares else 'Nenhum'}")
+print(f"C) Os números pares digitados foram: {pares if pares else 'Nenhum'}")'''
+
+# Versão do Guanabara
+
+núm = (int(input('Digite um número: ')),
+       int(input('Digite outro número: ')),
+       int(input('Digite mais um número: ')),
+       int(input('Digite o último número: ')))
+print(f'Você digitou os valores {núm}')
+print(f'O valor 9 apareceu {núm.count(9)} vezes')
+if 3 in núm:
+    print(f'O valor 3 apareceu na {núm.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for n in núm:
+    if n % 2 == 0:
+        print(n, end='')
--- ex076.py
+++ ex076.py
@@ -1,4 +1,4 @@
-# Tupla com produtos e preços (nome, preço, nome, preço ...)
+'''# Tupla com produtos e preços (nome, preço, nome, preço ...)
 produtos = ('Arroz', 8.56,
             'Feijão',22.50,
             'Macarrão', 9.59,
@@ -20,4 +20,25 @@ for i in range(0, len(produtos), 2):
     preco = produtos[i + 1]
     print(f'{nome:.<30} R$ {preco:>6.2f}')
 
+print('-' * 40)'''
+
+# Versão do Guanabara
+
+listagem  = ('Lápis', 1.75,
+             'Borracha', 2,
+            'Caderno', 15.90,
+             'Estojo', 25,
+             'Transferidor', 4.20,
+             'Compasso', 9.99,
+             'Mochila', 120.32,
+             'Canetas', 22.30,
+            'Livro', 34.90)
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end= '')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
 print('-' * 40)
