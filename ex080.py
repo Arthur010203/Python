fd4a820f80ec719d3ccc2cc6f4c7eaea8cec9db5
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-11 16:40:49 -0300

    Python

--- ex080.py
+++ ex080.py
@@ -1,4 +1,4 @@
-# Lista para armazenar os valores em ordem
+''''# Lista para armazenar os valores em ordem
 valores = []
 
 for i in range(5):
@@ -19,4 +19,23 @@ for i in range(5):
             pos += 1
 
 # Mostrando a lista ordenada
-print(f"\nOs valores digitados em ordem foram: {valores}")
+print(f"\nOs valores digitados em ordem foram: {valores}")'''
+
+# Versão do Guanabara
+
+lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
