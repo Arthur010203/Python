1ddd5445af38552232c82fd5f0035d3c871a391d
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-11 16:58:20 -0300

    Python

--- ex081.py
+++ ex081.py
@@ -1,4 +1,4 @@
-# Lista para armazenar os números
+'''# Lista para armazenar os números
 valores = []
 
 while True:
@@ -20,6 +20,23 @@ tem_cinco = 5 in valores
 
 # Exibindo os resultados
 print("\nRESULTADOS:")
-print(f"A) Você digitou {quantidade} números.")
+print(f"A) Você digitou {quantidade} elementos.")
 print(f"B) Os valores em ordem decrescente: {valores_decrescente}")
-print(f"C) O valor 5 {'está' if tem_cinco else 'NÃO está'} na lista.")
+print(f"C) O valor 5 {'está' if tem_cinco else 'NÃO está'} na lista.")'''
+
+# Versão do Guanabara
+
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar ? [S/N]' ))
+    if resp in 'Nn' :
+        break
+print('-=' * 30)
+print(f'Você digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
