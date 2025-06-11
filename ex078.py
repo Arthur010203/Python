6d06466069d388c8d84d0449236b24bb32001da7
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-11 16:06:40 -0300

    Python

--- ex078.py
+++ ex078.py
@@ -1,4 +1,4 @@
-# Lista para armazenar os valores
+''''# Lista para armazenar os valores
 valores = []
 
 # Leitura de 5 valores numéricos
@@ -17,4 +17,31 @@ posicoes_menor = [i for i, v in enumerate(valores) if v == menor]
 # Exibindo os resultados
 print(f'\nVocê digitou os valores: {valores}')
 print(f'O maior valor digitado foi {maior} nas posições: {posicoes_maior}')
-print(f'O menor valor digitado foi {menor} nas posições: {posicoes_menor}')
+print(f'O menor valor digitado foi {menor} nas posições: {posicoes_menor}')'''
+
+# Versão do Guanabara
+
+listanum = []
+mai = 0
+men = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a Posição {c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('=-' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {mai} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}... ', end= '')
+print()
+print(f'O menor valor digitado foi {men} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end='')
+print()
