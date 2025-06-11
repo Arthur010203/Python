fd55761127fd0b335b92f97a6d4a92e8abddc1e3
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-11 17:14:59 -0300

    Python

--- ex082.py
+++ ex082.py
@@ -1,4 +1,4 @@
-# Lista principal e listas separadas para pares e ímpares
+'''# Lista principal e listas separadas para pares e ímpares
 valores = []
 pares = []
 impares = []
@@ -22,4 +22,24 @@ while True:
 print("\nRESULTADOS:")
 print(f"Lista completa: {valores}")
 print(f"Lista de pares: {pares}")
-print(f"Lista de ímpares: {impares}")
+print(f"Lista de ímpares: {impares}")'''
+
+# Versão do Guanabara
+
+num = list()
+pares = list()
+ímpares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        ímpares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de pares é {ímpares}')
