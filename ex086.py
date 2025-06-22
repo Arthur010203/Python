0aee5fb587c9be32508c94e2bfe760e7778bb978
Arthur <arthurfreitasdearaujo@outlook.com>
2025-06-22 16:49:20 -0300

    Python

--- ex086.py
+++ ex086.py
@@ -1,4 +1,4 @@
-# Criando a matriz vazia
+''''# Criando a matriz vazia
 matriz = [[], [], []]
 
 # Preenchendo a matriz com valores do teclado
@@ -12,4 +12,16 @@ print("\nMatriz 3x3:")
 for linha in matriz:
     for valor in linha:
         print(f"[ {valor:^5} ]", end =' ')
+    print()'''
+
+# Versão do Guanabara
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l] [c]:^5}]', end='')
     print()
+
