698a1522819484a6fe9a3599294c998b3e78200f
Arthur <arthurfreitasdearaujo@outlook.com>
2025-07-26 11:27:48 -0300

    Python

--- ex097.py
+++ ex097.py
@@ -1,10 +1,10 @@
-def escreva(texto):
-    '''
+'''def escreva(texto):
+
     Exibe uma mensagem com uma moldura de traços adaptável ao temanho do texto
 
     Parâmetro:
     texto (str): A mensagem que será exibida.
-    '''
+
 
     tamanho = len(texto)
     print('-' * (tamanho + 4))
@@ -14,4 +14,17 @@ def escreva(texto):
 # Programa principal
 escreva('Olá, Mundo!')
 escreva('Programação em Python')
-escreva('Arthur')
+escreva('Arthur')'''
+
+# Versão do Guanabara
+
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'    {msg}')
+    print('~'*tam)
+
+# Programa Principal
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
