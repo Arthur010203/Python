ab22c31edbcb6bb5c2cfff61fdebfb8090a01e88
Arthur <arthurfreitasdearaujo@outlook.com>
2025-07-26 12:21:51 -0300

    Python

--- ex099.py
+++ ex099.py
@@ -1,4 +1,4 @@
-def maior(*numeros):
+'''def maior(*numeros):
     if len(numeros) == 0:
         print("Nenhum número foi informado")
         return
@@ -15,4 +15,29 @@ def maior(*numeros):
 maior(1, 5, 3, 9, 2)
 maior(10, 20)
 maior()
-maior(7)
+maior(7)'''
+
+# Versão do Guanabara
+from time import sleep
+def maior(* núm):
+    cont = maior = 0
+    print('-=' * 30)
+    print('Analisando os valores passados... ')
+    for valor in núm:
+        print(f'{valor} ', end='', flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'O maior valor informado foi {maior}. ')
+
+# Programa principal
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
