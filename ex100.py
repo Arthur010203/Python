2a01ff8dbeed67f675b639aabe6f30790f829afe
Arthur <arthurfreitasdearaujo@outlook.com>
2025-07-27 12:15:38 -0300

    Python

--- ex100.py
+++ ex100.py
@@ -1,4 +1,4 @@
-import random
+'''import random
 # Lista onde os números sorteados serão armazenados
 numeros = []
 
@@ -18,4 +18,28 @@ def somaPar():
 
 # Execução do programa
 sorteia()
-somaPar()
+somaPar()'''
+
+# Versão do Guanabara
+from random import randint
+from time import sleep
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for cont in range(0,5):
+        n = randint(1,10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+números = list()
+sorteia(números)
+somaPar(números)
