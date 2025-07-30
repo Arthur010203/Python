39dcef0412ccde278748769d23c060def207d75d
Arthur <arthurfreitasdearaujo@outlook.com>
2025-07-30 18:50:50 -0300

    Python

---
+++ ex103.py
@@ -0,0 +1,17 @@
+def ficha(jogador='<desconecido>', gols=0):
+    print(f'O jogador {jogador} fez {gols} gol(s) no campeonato.')
+
+# Programa principal
+nome = input('Nome do jogador: ').strip()
+gols = input('Número de gols: ').strip()
+
+# Tratando os dados
+if nome == '':
+    nome = '<desconhecido>'
+
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+
+ficha(nome, gols)
