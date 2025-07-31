1695203ef0559c2478d8ffdf792a44ec3a8d9ea8
Arthur <arthurfreitasdearaujo@outlook.com>
2025-07-31 20:38:28 -0300

    Python

--- ex101.py
+++ ex101.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-def voto(ano_nascimento):
+'''def voto(ano_nascimento):
     idade = date.today().year - ano_nascimento
     if idade < 16:
         return "VOTO NEGADO"
@@ -12,4 +12,20 @@ def voto(ano_nascimento):
 # Programa principal
 nascimento = int(input("Digite o ano de nascimento: "))
 situacao = voto(nascimento)
-print(f"Para quem nasceu em {nascimento}, a situação do voto é: {situacao}")
+print(f"Para quem nasceu em {nascimento}, a situação do voto é: {situacao}")'''
+
+# Versão do Guanabara
+
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+nasc = int(input("Em que ano você nasceu? "))
+print(voto(nasc))
