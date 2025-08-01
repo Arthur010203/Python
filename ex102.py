a1b55d063d9b1ac0d1f85406502936b701abc6b4
Arthur <arthurfreitasdearaujo@outlook.com>
2025-08-01 12:15:34 -0300

    Python

--- ex102.py
+++ ex102.py
@@ -1,4 +1,4 @@
-def fatorial(num, show=False):
+'''def fatorial(num, show=False):
     """
     Calcula o fatorial de um número.
 
@@ -20,3 +20,27 @@ def fatorial(num, show=False):
 # Exemplo de uso:
 n = int(input("Digite um número: "))
 print(fatorial(n, show=True)) # Coloque show=False se não quiser mostrar o cálculo
+'''
+
+# Versão do Guanabara
+
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n:  O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+# Programa Prinicipal
+#print(fatorial(5))
+help(fatorial)
