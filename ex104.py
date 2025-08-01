836cc8793636f426b25ba1903f7537e9a87a8568
Arthur <arthurfreitasdearaujo@outlook.com>
2025-08-01 12:38:43 -0300

    Python

--- ex104.py
+++ ex104.py
@@ -1,4 +1,5 @@
-def leiaInt(msg):
+
+'''def leiaInt(msg):
     while True:
         try:
             n = int(input(msg))
@@ -8,5 +9,24 @@ def leiaInt(msg):
 
 # Exemplo de uso:
 n = leiaInt('Digite um número: ')
-print(f'Você digitou o número {n}.')
+print(f'Você digitou o número {n}.')'''
 
+# Versão do Guanabara
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+# Programa principal
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
