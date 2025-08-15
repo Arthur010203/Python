02067b279e1afe9edd67e911d9f8c129869358f6
Arthur <arthurfreitasdearaujo@outlook.com>
2025-08-15 19:38:35 -0300

    Python

--- ex107/main.py
+++ ex107/main.py
@@ -1,4 +1,5 @@
-import moeda
+
+"""import moeda
 
 preco = float(input("Digite o preço: R$ "))
 
@@ -6,3 +7,11 @@ print(f"O dobro de R${preco:.2f} é R${moeda.dobro(preco):.2f}")
 print(f"A metade de R${preco:.2f} é R${moeda.metade(preco):.2f}")
 print(f"Aumentando 10%, temos R${moeda.aumentar(preco, 10):.2f}")
 print(f"Diminuindo 13%, temos R${moeda.diminuir(preco, 13):.2f}")
+"""
+
+from ex107 import moeda
+
+p = float(input('Digite o preço: R$'))
+print(f'A metade de R${p} é R${moeda.metade(p)}')
+print(f'O dobro de R${p} é R${moeda.dobro(p)}')
+print(f'Aumentar 10%, temos R${moeda.aumentar(p, 10)}')
--- ex107/moeda.py
+++ ex107/moeda.py
@@ -1,31 +1,49 @@
-def aumentar(preco=0, taxa=0):
-    """
+"""def aumentar(preco=0, taxa=0):
+
     Docstring
     Aumenta o preço em uma determinada porcentagem.
     :param preco: Valor original
     :param taxa: Porcentagem de aumento
     :return: Novo preço com aumento
-    """
+
     return preco + (preco * taxa / 100)
 
 def diminuir(preco=0, taxa=0):
-    """
+
     Docstrings
     Diminui o preço em uma determinada porcentagem.
     :param preco:  Valor original
     :param taxa: Porcentagem de redução
     :return: Novo preço com desconto
-    """
+
     return preco - (preco * taxa / 100)
 
 def dobro(preco=0):
-    """
+
     Retorna o dobro do preço.
-    """
+
     return preco * 2
 
 def metade (preco=0):
-    """
+
     Retorna a metade do preço.
-    """
-    return preco / 2
+
+    return preco / 2"""
+
+# Versão do Guanabara
+
+def aumentar(preço, taxa):
+    res = preço + (preço * taxa/100)
+    return res
+
+def diminuir(preço, taxa):
+    res = preço - (preço * taxa/100)
+    return res
+
+def dobro(preço):
+    res = preço * 2
+    return res
+
+def metade(preço):
+    res = preço / 2
+    return res
