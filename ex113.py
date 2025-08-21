8d317edc1c3701047918fdf39c2bb4fe294ff5b6
Arthur <arthurfreitasdearaujo@outlook.com>
2025-08-21 19:13:18 -0300

    Python

---
+++ ex113.py
@@ -0,0 +1,30 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[0;31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        else:
+            return n
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except(ValueError, TypeError) :
+            print('\033[0;31mERRO! Digite um número real válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[0;31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0.0
+        else:
+            return n
+
+# Programa principal
+n1 = leiaInt('Digite um número inteiro: ')
+n2 = leiaFloat('Digite um número real: ')
+print(f'O valor inteiro digitado foi {n1} e o real foi {n2}')
